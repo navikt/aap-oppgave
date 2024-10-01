5f6a5b44193fb62f7619f4e0714bdae2e2037243
Frode Lindås <frode.lindas@nav.no>
2024-10-01 08:59:30 +0200

    Oppdatert versjoner på kelvin komponenter og behandflyt-kontrakt.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 val ktorVersion = "2.3.12"
 val komponenterVersjon = "0.0.84"
 val tilgangVersjon = "0.0.11"
-val behandlingsflytVersjon= "0.0.11"
+val behandlingsflytVersjon= "0.0.13"
 
 plugins {
     id("oppgave.conventions")
