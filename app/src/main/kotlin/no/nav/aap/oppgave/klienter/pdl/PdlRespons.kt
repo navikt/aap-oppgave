5fa5ea3c3d7c918b0ef8781d47007af1873b1d52
Frode Lindås <frode.lindas@nav.no>
2024-12-04 15:02:41 +0100

    Fjerner ubrukt kode.

--- app/src/main/kotlin/no/nav/aap/oppgave/klienter/pdl/PdlRespons.kt
+++ app/src/main/kotlin/no/nav/aap/oppgave/klienter/pdl/PdlRespons.kt
@@ -2,7 +2,6 @@ package no.nav.aap.oppgave.klienter.pdl
 
 import no.nav.aap.postmottak.klient.graphql.GraphQLError
 import no.nav.aap.postmottak.klient.graphql.GraphQLExtensions
-import java.time.LocalDate
 
 internal data class PdlResponse(
     val data: PdlData?,
@@ -11,18 +10,9 @@ internal data class PdlResponse(
 )
 
 data class PdlData(
-//    val hentPersonBolk: List<HentPersonBolkResult>? = null,
-//    val hentIdenter: HentIdenterResult? = null,
     val hentGeografiskTilknytning: GeografiskTilknytning? = null
 )
 
-/*
-data class HentPersonBolkResult(
-    val ident: String,
-    val person: PdlPerson?,
-    val code: String,
-)
- */
 
 data class GeografiskTilknytning(
     val gtType: GeografiskTilknytningType,
@@ -38,41 +28,4 @@ enum class GeografiskTilknytningType{
     UDEFINERT
 }
 
-/*
-data class PdlPerson(
-    val navn: List<Navn>, val code: Code?     //Denne er påkrevd ved hentPersonBolk
-)
-*/
-
-/*
-data class Fødselsdato(val foedselsdato: LocalDate)
-
-enum class Code {
-    ok, not_found, bad_request //TODO: add more
-}
-*/
-/*
-data class Navn(
-    val fornavn: String?,
-    val mellomnavn: String?,
-    val etternavn: String?
-) {
-    fun fulltNavn(): String {
-        return "${fornavn ?: ""} ${mellomnavn ?: ""} ${etternavn ?: ""}".trim()
-    }
-}
-
-data class HentIdenterResult(val identer: List<PdlIdent>)
-data class PdlIdent(
-    val ident: String,
-    val historisk: Boolean,
-    val gruppe: PdlGruppe
-)
-
-enum class PdlGruppe {
-    FOLKEREGISTERIDENT,
-    AKTORID,
-    NPID,
-}
-*/
 
