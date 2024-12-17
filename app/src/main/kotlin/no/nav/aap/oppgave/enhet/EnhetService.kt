b6232bf298c991afc617afca257edb6ae42c2b83
Frode Lindås <frode.lindas@nav.no>
2024-12-17 15:48:24 +0100

    Legger på litt debug på henting av enheter.

--- app/src/main/kotlin/no/nav/aap/oppgave/enhet/EnhetService.kt
+++ app/src/main/kotlin/no/nav/aap/oppgave/enhet/EnhetService.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.oppgave.enhet
 
 import no.nav.aap.oppgave.klienter.msgraph.IMsGraphClient
+import org.slf4j.LoggerFactory
 import kotlin.collections.filter
 import kotlin.collections.map
 import kotlin.text.removePrefix
@@ -8,7 +9,12 @@ import kotlin.text.startsWith
 
 class EnhetService(private val msGraphClient: IMsGraphClient) {
 
+    private val log = LoggerFactory.getLogger(EnhetService::class.java)
+
     suspend fun hentEnheter(currentToken: String, ident: String): List<String> {
+        val groups = msGraphClient.hentAdGrupper(currentToken, ident).groups
+            .map {it.name}
+        log.info("###### Groups: $groups")
         return msGraphClient.hentAdGrupper(currentToken, ident).groups
             .filter { it.name.startsWith(ENHET_GROUP_PREFIX) }
             .map { it.name.removePrefix(ENHET_GROUP_PREFIX) }
