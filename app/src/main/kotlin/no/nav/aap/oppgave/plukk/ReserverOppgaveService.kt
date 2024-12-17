11096a9d788382b70838796521053fdc3b463dee
Fredrik Meyer <fredrik.meyer@nav.no>
2024-12-17 17:13:57 +0100

    Eksempel på bruk

--- app/src/main/kotlin/no/nav/aap/oppgave/plukk/ReserverOppgaveService.kt
+++ app/src/main/kotlin/no/nav/aap/oppgave/plukk/ReserverOppgaveService.kt
@@ -2,9 +2,14 @@ package no.nav.aap.oppgave.plukk
 
 import no.nav.aap.komponenter.dbconnect.DBConnection
 import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.OidcToken
+import no.nav.aap.komponenter.httpklient.json.DefaultJsonMapper
+import no.nav.aap.motor.FlytJobbRepository
+import no.nav.aap.motor.JobbInput
 import no.nav.aap.oppgave.AvklaringsbehovReferanseDto
 import no.nav.aap.oppgave.OppgaveRepository
 import no.nav.aap.oppgave.OppgaveId
+import no.nav.aap.oppgave.prosessering.StatistikkHendelseJobb
+import no.nav.aap.oppgave.statistikk.HendelseType
 
 class ReserverOppgaveService(val connection: DBConnection) {
 
@@ -22,9 +27,12 @@ class ReserverOppgaveService(val connection: DBConnection) {
 
         val harTilgang = TilgangGateway.sjekkTilgang(avklaringsbehovReferanse, token)
         if (harTilgang) {
-            val oppgaveRepo = OppgaveRepository(connection)
             oppgaverSomSkalReserveres.forEach {
                 oppgaveRepo.reserverOppgave(it, ident, ident)
+                FlytJobbRepository(connection).leggTil(
+                    JobbInput(StatistikkHendelseJobb).medParameter("hendelsesType", HendelseType.RESERVERT.name)
+                        .medPayload(DefaultJsonMapper.toJson(it))
+                )
             }
             return oppgaverSomSkalReserveres
         }
