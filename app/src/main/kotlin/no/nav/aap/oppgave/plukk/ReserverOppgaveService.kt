2951c704f2bc5c306911ba85ad5d20cad6ee5ec4
Frode Lindås <frode.lindas@nav.no>
2025-01-02 07:32:18 +0100

    Oppdatert til siste kelvin komponenter versjon, men noen små endringer på imports.

--- app/src/main/kotlin/no/nav/aap/oppgave/plukk/ReserverOppgaveService.kt
+++ app/src/main/kotlin/no/nav/aap/oppgave/plukk/ReserverOppgaveService.kt
@@ -2,15 +2,10 @@ package no.nav.aap.oppgave.plukk
 
 import no.nav.aap.komponenter.dbconnect.DBConnection
 import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.OidcToken
-import no.nav.aap.komponenter.httpklient.json.DefaultJsonMapper
-import no.nav.aap.motor.FlytJobbRepository
-import no.nav.aap.motor.JobbInput
 import no.nav.aap.oppgave.AvklaringsbehovReferanseDto
 import no.nav.aap.oppgave.OppgaveRepository
 import no.nav.aap.oppgave.OppgaveId
-import no.nav.aap.oppgave.prosessering.StatistikkHendelseJobb
 import no.nav.aap.oppgave.prosessering.sendOppgaveStatusOppdatering
-import no.nav.aap.oppgave.server.authenticate.ident
 import no.nav.aap.oppgave.statistikk.HendelseType
 
 class ReserverOppgaveService(val connection: DBConnection) {
