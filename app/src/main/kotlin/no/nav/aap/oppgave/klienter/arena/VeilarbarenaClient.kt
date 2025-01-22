00a7f3735f2645a9735d3c8754c2d8fd9855c2fc
Frode Lindås <frode.lindas@nav.no>
2025-01-22 11:07:29 +0100

    Fjerner unødvendig logging

--- app/src/main/kotlin/no/nav/aap/oppgave/klienter/arena/VeilarbarenaClient.kt
+++ app/src/main/kotlin/no/nav/aap/oppgave/klienter/arena/VeilarbarenaClient.kt
@@ -35,7 +35,6 @@ class VeilarbarenaClient {
     )
 
     fun hentOppfølgingsenhet(personIdent: String): String? {
-        log.info("Finner oppfølgingsenhet for fnr: $personIdent")
         val hentStatusUrl = url.resolve("/veilarbarena/api/v2/arena/hent-status")
         val request = PostRequest(
             body = HentOppfølgingsenhetRequest(personIdent),
