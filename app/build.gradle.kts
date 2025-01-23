f4a440072b67bad692485db22c615ea4ab1a4eb4
Frode Lindås <frode.lindas@nav.no>
2025-01-23 15:42:13 +0100

    Oppdatert avhengigheter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val ktorVersion = "3.0.3"
 val komponenterVersjon = "1.0.118"
-val tilgangVersjon = "0.0.89"
-val behandlingsflytVersjon = "0.0.114"
-val postmottakVersjon = "0.0.24"
+val tilgangVersjon = "0.0.92"
+val behandlingsflytVersjon = "0.0.125"
+val postmottakVersjon = "0.0.28"
 
 plugins {
     id("oppgave.conventions")
