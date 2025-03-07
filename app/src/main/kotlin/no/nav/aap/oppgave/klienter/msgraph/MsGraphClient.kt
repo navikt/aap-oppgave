d67c213ad7d2c844d2c004b234d74c3874c05777
Fredrik Meyer <fredrik.meyer@nav.no>
2025-03-07 08:03:15 +0100

    Liten typo i miljøvariabel

--- app/src/main/kotlin/no/nav/aap/oppgave/klienter/msgraph/MsGraphClient.kt
+++ app/src/main/kotlin/no/nav/aap/oppgave/klienter/msgraph/MsGraphClient.kt
@@ -1,14 +1,6 @@
 package no.nav.aap.oppgave.klienter.msgraph
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.serialization.jackson.*
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import no.nav.aap.komponenter.config.requiredConfigForKey
 import no.nav.aap.komponenter.httpklient.httpclient.ClientConfig
@@ -61,20 +53,3 @@ data class Group(
     val name: String
 )
 
-internal object HttpClientFactory {
-    fun create(): HttpClient = HttpClient(CIO) {
-        install(HttpTimeout) {
-            requestTimeoutMillis = 25_000
-            connectTimeoutMillis = 5_000
-        }
-        install(HttpRequestRetry)
-
-        install(ContentNegotiation) {
-            jackson {
-                disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                registerModule(JavaTimeModule())
-            }
-        }
-    }
-}
