b52be93be9323312da2b2fff3b091eafce122513
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-03-25 07:10:15 +0100

    Revert "Midlertidig fiks: Tolk exception fra tilgang som false"
    This reverts commit 02e391ed2df84a562955c47040f71b705acd4780.

--- app/src/main/kotlin/no/nav/aap/oppgave/plukk/TilgangGateway.kt
+++ app/src/main/kotlin/no/nav/aap/oppgave/plukk/TilgangGateway.kt
@@ -13,43 +13,35 @@ import no.nav.aap.tilgang.BehandlingTilgangRequest
 import no.nav.aap.tilgang.JournalpostTilgangRequest
 import no.nav.aap.tilgang.Operasjon
 import no.nav.aap.tilgang.TilgangResponse
-import org.slf4j.LoggerFactory
 import java.net.URI
 
 object TilgangGateway {
     private val baseUrl = URI.create(requiredConfigForKey("integrasjon.tilgang.url"))
     private val config = ClientConfig(scope = requiredConfigForKey("integrasjon.tilgang.scope"))
-    private val log = LoggerFactory.getLogger(TilgangGateway::class.java)
 
     private val client = RestClient.withDefaultResponseHandler(
         config = config,
         tokenProvider = OnBehalfOfTokenProvider,
         prometheus = prometheus
     )
-    
+
     fun sjekkTilgang(avklaringsbehovReferanse: AvklaringsbehovReferanseDto, token: OidcToken): Boolean {
-        try {
-            return if (avklaringsbehovReferanse.journalpostId != null) {
-                harTilgangTilJournalpost(
-                    JournalpostTilgangRequest(
-                        journalpostId = avklaringsbehovReferanse.journalpostId!!,
-                        avklaringsbehovKode = avklaringsbehovReferanse.avklaringsbehovKode,
-                        operasjon = Operasjon.SAKSBEHANDLE
-                    ), token
-                )
-            } else {
-                harTilgangTilBehandling(
-                    BehandlingTilgangRequest(
-                        behandlingsreferanse = avklaringsbehovReferanse.referanse!!,
-                        avklaringsbehovKode = avklaringsbehovReferanse.avklaringsbehovKode,
-                        operasjon = Operasjon.SAKSBEHANDLE
-                    ), token
-                )
-            }
-        } catch (e: Exception) {
-            // TODO: Undersøk håndtering av dette - bør kanskje ikke alltid returnere false da dette kan føre til unødvendig mange kall til tilgang
-            log.info("Fikk feil mot tilgang: $e - returnerer false")
-            return false
+        return if (avklaringsbehovReferanse.journalpostId != null) {
+            harTilgangTilJournalpost(
+                JournalpostTilgangRequest(
+                    journalpostId = avklaringsbehovReferanse.journalpostId!!,
+                    avklaringsbehovKode = avklaringsbehovReferanse.avklaringsbehovKode,
+                    operasjon = Operasjon.SAKSBEHANDLE
+                ), token
+            )
+        } else {
+            harTilgangTilBehandling(
+                BehandlingTilgangRequest(
+                    behandlingsreferanse = avklaringsbehovReferanse.referanse!!,
+                    avklaringsbehovKode = avklaringsbehovReferanse.avklaringsbehovKode,
+                    operasjon = Operasjon.SAKSBEHANDLE
+                ), token
+            )
         }
     }
 
