1bfa0baaaf83a69d1a5a9dc7d0064d6dcbec142e
Matias Vinjevoll <matias.vinjevoll@nav.no>
2025-05-13 12:00:58 +0200

    Bump behandlingsflytversjon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 val ktorVersion = "3.1.3"
 val komponenterVersjon = "1.0.242"
 val tilgangVersjon = "1.0.64"
-val behandlingsflytVersjon = "0.0.289"
+val behandlingsflytVersjon = "0.0.294"
 val postmottakVersjon = "0.0.90"
 
 plugins {
