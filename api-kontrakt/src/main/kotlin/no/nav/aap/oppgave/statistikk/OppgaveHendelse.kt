3f7deb8b31cbbf12490ceb1950e377200f8afb36
Fredrik Meyer <fredrik.meyer@nav.no>
2025-06-20 14:20:28 +0200

    Trenger ikke være nullable lenger

--- api-kontrakt/src/main/kotlin/no/nav/aap/oppgave/statistikk/OppgaveHendelse.kt
+++ api-kontrakt/src/main/kotlin/no/nav/aap/oppgave/statistikk/OppgaveHendelse.kt
@@ -24,7 +24,7 @@ data class OppgaveHendelse(
     val hendelse: HendelseType,
     @Deprecated("Bruk oppgaveTilStatistikkDto i stedet")
     val oppgaveDto: OppgaveDto,
-    val oppgaveTilStatistikkDto: OppgaveTilStatistikkDto? = null
+    val oppgaveTilStatistikkDto: OppgaveTilStatistikkDto
 )
 
 /**
