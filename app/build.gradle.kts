b51142340b43f3275eaa7297e696e95409da0fd5
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-07-10 08:48:50 +0200

    Oppdaterer versjon av behandlingsflyt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 val ktorVersion = "3.2.1"
 val komponenterVersjon = "1.0.280"
 val tilgangVersjon = "1.0.93"
-val behandlingsflytVersjon = "0.0.363"
+val behandlingsflytVersjon = "0.0.371"
 val postmottakVersjon = "0.0.96"
 
 plugins {
