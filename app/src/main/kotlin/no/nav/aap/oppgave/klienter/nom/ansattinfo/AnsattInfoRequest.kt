8f3626208ef1a1529930eece4e443fb058637eb1
SelmaBergstrand <selma.kristine@gmail.com>
2025-09-04 12:38:56 +0200

    AAP-1326 ansattinfovariables -> variables

--- app/src/main/kotlin/no/nav/aap/oppgave/klienter/nom/ansattinfo/AnsattInfoRequest.kt
+++ app/src/main/kotlin/no/nav/aap/oppgave/klienter/nom/ansattinfo/AnsattInfoRequest.kt
@@ -1,7 +1,7 @@
 package no.nav.aap.oppgave.klienter.nom.ansattinfo
 
 
-data class AnsattInfoRequest(val query: String, val ansattInfoVariables: AnsattInfoVariables)
+data class AnsattInfoRequest(val query: String, val variables: AnsattInfoVariables)
 
 data class AnsattInfoVariables(val navIdent: String)
 
