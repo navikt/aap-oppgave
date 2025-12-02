fe1405b1851c693f85bc14a9e7452b7abe03c062
SelmaBergstrand <selma.kristine@gmail.com>
2025-12-02 14:46:40 +0100

    Fjern featuretoggle

--- app/src/main/kotlin/no/nav/aap/oppgave/AvklaringsbehovForRolle.kt
+++ app/src/main/kotlin/no/nav/aap/oppgave/AvklaringsbehovForRolle.kt
@@ -3,22 +3,12 @@ package no.nav.aap.oppgave
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon.BehovType
 import no.nav.aap.behandlingsflyt.kontrakt.steg.StegType
 import no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon
-import no.nav.aap.oppgave.unleash.FeatureToggles
-import no.nav.aap.oppgave.unleash.IUnleashService
-import no.nav.aap.oppgave.unleash.UnleashServiceProvider
 import no.nav.aap.postmottak.kontrakt.avklaringsbehov.Definisjon as PostmottakDefinisjon
 import no.nav.aap.tilgang.Rolle
 
-private val unleashService: IUnleashService = UnleashServiceProvider.provideUnleashService()
-private val inkluderteBehovstyper = if (unleashService.isEnabled(FeatureToggles.InkluderOverstyrteAvklaringsbehov)) {
-    setOf(BehovType.MANUELT_PÅKREVD, BehovType.MANUELT_FRIVILLIG, BehovType.OVERSTYR)
-} else {
-    setOf(BehovType.MANUELT_PÅKREVD, BehovType.MANUELT_FRIVILLIG)
-}
-
 val AVKLARINGSBEHOV_FOR_VEILEDER =
     Definisjon.entries
-        .filter { it.type in inkluderteBehovstyper }
+        .filter { it.type in setOf(BehovType.MANUELT_PÅKREVD, BehovType.MANUELT_FRIVILLIG, BehovType.OVERSTYR) }
         .filter { Rolle.SAKSBEHANDLER_OPPFOLGING in it.løsesAv }
         .map { AvklaringsbehovKode(it.kode.name) }
         .toSet()
@@ -26,7 +16,7 @@ val AVKLARINGSBEHOV_FOR_VEILEDER =
 val AVKLARINGSBEHOV_FOR_SAKSBEHANDLER =
     Definisjon.entries
         .asSequence()
-        .filter { it.type in inkluderteBehovstyper }
+        .filter { it.type in setOf(BehovType.MANUELT_PÅKREVD, BehovType.MANUELT_FRIVILLIG, BehovType.OVERSTYR) }
         .filter { Rolle.SAKSBEHANDLER_NASJONAL in it.løsesAv }
         .filter { it.løsesISteg != StegType.KVALITETSSIKRING }
         .map { AvklaringsbehovKode(it.kode.name) }
@@ -34,13 +24,13 @@ val AVKLARINGSBEHOV_FOR_SAKSBEHANDLER =
 
 val AVKLARINGSBEHOV_FOR_VEILEDER_OG_SAKSBEHANDLER =
     Definisjon.entries
-        .filter { it.type in inkluderteBehovstyper }
+        .filter { it.type in setOf(BehovType.MANUELT_PÅKREVD, BehovType.MANUELT_FRIVILLIG, BehovType.OVERSTYR) }
         .filter { Rolle.SAKSBEHANDLER_OPPFOLGING in it.løsesAv && Rolle.SAKSBEHANDLER_NASJONAL in it.løsesAv }
         .map { AvklaringsbehovKode(it.kode.name) }
         .toSet()
 
 val AVKLARINGSBEHOV_FOR_BESLUTTER = Definisjon.entries
-    .filter { it.type in inkluderteBehovstyper }
+    .filter { it.type in setOf(BehovType.MANUELT_PÅKREVD, BehovType.MANUELT_FRIVILLIG, BehovType.OVERSTYR) }
     .filter { Rolle.BESLUTTER in it.løsesAv }
     .map { AvklaringsbehovKode(it.kode.name) }
     .toSet()
