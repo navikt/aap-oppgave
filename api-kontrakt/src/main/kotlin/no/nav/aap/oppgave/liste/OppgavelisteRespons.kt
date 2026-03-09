bdb42634511b134b4c99723ddb7d52077dd4e6b6
HenrikBuggeHalvorsen <henrik.bugge.halvorsen@nav.no>
2026-03-09 12:50:11 +0100

    returnér behandlingstyper utledet av innsendt oppgavefilter fra backend, slik at FE kan forhåndsvise dette

--- api-kontrakt/src/main/kotlin/no/nav/aap/oppgave/liste/OppgavelisteRespons.kt
+++ api-kontrakt/src/main/kotlin/no/nav/aap/oppgave/liste/OppgavelisteRespons.kt
@@ -1,9 +1,11 @@
 package no.nav.aap.oppgave.liste
 
 import no.nav.aap.oppgave.OppgaveDto
+import no.nav.aap.oppgave.verdityper.Behandlingstype
 
 data class OppgavelisteRespons(
     val antallTotalt: Int,
     val oppgaver: List<OppgaveDto>,
-    val antallGjenstaaende: Int? = null
+    val antallGjenstaaende: Int? = null,
+    val sattFilterBehandlingstyper: Set<Behandlingstype>? = emptySet(),
 )
