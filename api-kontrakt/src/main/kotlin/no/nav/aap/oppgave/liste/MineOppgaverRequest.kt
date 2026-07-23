bfb66d01681bb0b1322de60525aadb83c575e7ce
SelmaBergstrand <selma.kristine.bergstrand@nav.no>
2026-07-23 11:43:22 +0200

    AAP-2235 kan ikke fjerne queryparam likevel

--- api-kontrakt/src/main/kotlin/no/nav/aap/oppgave/liste/MineOppgaverRequest.kt
+++ api-kontrakt/src/main/kotlin/no/nav/aap/oppgave/liste/MineOppgaverRequest.kt
@@ -1,7 +1,9 @@
 package no.nav.aap.oppgave.liste
 
+import com.papsign.ktor.openapigen.annotations.parameters.QueryParam
+
 data class MineOppgaverRequest(
-    val kunPaaVent: Boolean? = false,
-    val sortby: OppgaveSorteringFelt? = null,
-    val sortorder: OppgaveSorteringRekkefølge? = null,
+    @param:QueryParam("Vis kun på vent-oppgaver.") val kunPaaVent: Boolean? = false,
+    @param:QueryParam("Sorter oppgaveliste") val sortby: OppgaveSorteringFelt? = null,
+    @param:QueryParam("Sorteringsrekkefølge") val sortorder: OppgaveSorteringRekkefølge? = null,
 )
